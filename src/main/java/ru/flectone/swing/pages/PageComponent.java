7060a633478d9037316f1927ec58a1c8f5c1e3eb
TheFaser <artem.mochalin11@gmail.com>
2022-09-16 20:22:13 +0500

    Fix rp dimension

--- src/main/java/ru/flectone/swing/pages/PageComponent.java
+++ src/main/java/ru/flectone/swing/pages/PageComponent.java
@@ -40,7 +40,7 @@ public class PageComponent extends PageDefault {
         box.add(createRigidArea(0, 5));
         box.add(createLabel(description));
 
-        box.setPreferredSize(new Dimension(500, 40));
+        box.setPreferredSize(new Dimension(500, 50));
         add(box);
         setName(page);
     }
@@ -71,7 +71,7 @@ public class PageComponent extends PageDefault {
         box.add(createCheckBox(checkBox, page));
         box.add(createRigidArea(0, 5));
         box.add(createLabel(description));
-        box.setPreferredSize(new Dimension(440, 70));
+        box.setPreferredSize(new Dimension(384, 70));
         add(box);
     }
 
